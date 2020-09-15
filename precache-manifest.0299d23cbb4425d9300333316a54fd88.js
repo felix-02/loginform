d066c9155b65afaa2414652e1fd5a3a2dce56fe4
pulkit <pulkit1298@gmail.com>
2020-09-15 20:00:13 +0530

    Updates

---
+++ precache-manifest.0299d23cbb4425d9300333316a54fd88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229ab9d416a0aa9f8922cdf3f4948116",
+    "url": "/loginform/index.html"
+  },
+  {
+    "revision": "ee806daedded0a9b472e",
+    "url": "/loginform/static/css/main.08317260.chunk.css"
+  },
+  {
+    "revision": "041a526ca2458b41a9fb",
+    "url": "/loginform/static/js/2.c41840f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loginform/static/js/2.c41840f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee806daedded0a9b472e",
+    "url": "/loginform/static/js/main.16f15b70.chunk.js"
+  },
+  {
+    "revision": "03c77ac1abf2b82a05f9",
+    "url": "/loginform/static/js/runtime-main.0ab9ea98.js"
+  },
+  {
+    "revision": "dccff639f85d0f51fd5374900abc3fdb",
+    "url": "/loginform/static/media/image.dccff639.jpg"
+  }
+]);
